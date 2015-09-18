b4bd7c698da08f67725437438354d15285c3c44b
gau1991 <gaurav.astikar@rtcamp.com>
2015-09-18 15:35:09 +0530

    Removed EasyEngine as it is allready installed

--- provision/provision.sh
+++ provision/provision.sh
@@ -10,8 +10,6 @@ sudo echo -e "[user]\n\tname = EasyEngine\n\temail = root@easyengine.com" > ~/.g
 
 myecho
 
-wget -qO ee rt.cx/ee && sudo bash ee  || exit 1
-
 myecho
 # Install latest Ruby
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
